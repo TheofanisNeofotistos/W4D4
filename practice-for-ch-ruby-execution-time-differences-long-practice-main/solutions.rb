b8a0a78bced5c17da7b683240a2849329fab1ce1
Theofanis Neofotistos <neofotistostheofanis@gmail.com>
2023-05-25 11:51:42 -0400

    Start for Largest_contiguous_subsum

--- practice-for-ch-ruby-execution-time-differences-long-practice-main/solutions.rb
+++ practice-for-ch-ruby-execution-time-differences-long-practice-main/solutions.rb
@@ -1,4 +1,7 @@
-# def my_min(array) #O(n^2)
+#O(n^2) - time 
+#O(1) - space
+
+# def my_min(array) 
 #     min = nil
 #     array.each do |num1|
 #         array.each do |num2|
@@ -13,6 +16,9 @@
 # array = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
 # p my_min(array)  # =>  -5
 
+#O(n) - time 
+#O(1) - space
+
 # def my_min(array) # O(n)
 #     min = nil
 #     array.each do |num|
@@ -25,3 +31,25 @@
 
 # array = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
 # p my_min(array)  # =>  -5
+
+def largest_contiguous_sub_sum(list)
+    sub_arrs = []
+    
+    list.each do |ele|
+        list.each do |ele2|
+            
+        end
+    end
+
+end
+
+list = [5, 3, -7]
+largest_contiguous_subsum(list) # => 8
+
+# possible sub-sums
+[5]           # => 5
+[5, 3]        # => 8 --> we want this one
+[5, 3, -7]    # => 1
+[3]           # => 3
+[3, -7]       # => -4
+[-7]          # => -7
