075ad3bd34d799eba7f02b248ccd842bfba2212e
Viktoria Czaran <vczaran@gmail.com>
2023-05-25 12:15:43 -0400

    Completed contiguous_subsum in n^2 time

--- practice-for-ch-ruby-execution-time-differences-long-practice-main/solutions.rb
+++ practice-for-ch-ruby-execution-time-differences-long-practice-main/solutions.rb
@@ -1,17 +1,17 @@
 #O(n^2) - time 
 #O(1) - space
 
-# def my_min(array) 
-#     min = nil
-#     array.each do |num1|
-#         array.each do |num2|
-#             if num1 < num2 && num1 < min.to_i
-#                 min = num1
-#             end
-#         end
-#     end
-#     min 
-# end 
+def my_min(array) 
+    min = nil
+    array.each do |num1|
+        array.each do |num2|
+            if num1 < num2 && num1 < min.to_i
+                min = num1
+            end
+        end
+    end
+    min 
+end 
 
 # array = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
 # p my_min(array)  # =>  -5
@@ -19,32 +19,43 @@
 #O(n) - time 
 #O(1) - space
 
-# def my_min(array) # O(n)
-#     min = nil
-#     array.each do |num|
-#         if num < min.to_i 
-#             min = num 
-#         end
-#     end
-#     min
-# end
+def my_min(array) # O(n)
+    min = nil
+    array.each do |num|
+        if num < min.to_i 
+            min = num 
+        end
+    end
+    min
+end
 
 # array = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
 # p my_min(array)  # =>  -5
 
-def largest_contiguous_sub_sum(list)
+# O(n^2) - time
+def largest_contiguous_subsum(list)
     sub_arrs = []
-    
-    list.each do |ele|
-        list.each do |ele2|
-            
+    i = 0
+
+    while i < list.length
+        sub_arrs << [list[i]]
+        j = i + 1
+        while j < list.length
+            sub_arrs << list[i..j]
+            j += 1
         end
+        i += 1
     end
 
+    sum = 0
+    sub_arrs.each do |arr|
+      sum = arr.sum if arr.sum > sum
+    end
+    sum
 end
 
 list = [5, 3, -7]
-largest_contiguous_subsum(list) # => 8
+p largest_contiguous_subsum(list) # => 8
 
 # possible sub-sums
 [5]           # => 5
