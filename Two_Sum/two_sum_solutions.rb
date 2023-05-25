b937c98b555ce53fa555525c525e37212f594c28
Viktoria Czaran <vczaran@gmail.com>
2023-05-25 17:08:45 -0400

    Stuck on okay_two_sum

--- Two_Sum/two_sum_solutions.rb
+++ Two_Sum/two_sum_solutions.rb
@@ -1,3 +1,4 @@
+#O(n^2) - time
 def two_sum_bf? (arr,target)
     arr.each_with_index do |num1,idx1|
         arr.each_with_index do |num2,idx2|
@@ -9,6 +10,32 @@ def two_sum_bf? (arr,target)
     false 
 end
 
+# arr = [0, 1, 5, 7]
+# p two_sum_bf?(arr, 6) # => should be true
+# p two_sum_bf?(arr, 10) # => should be false
+
+def okay_two_sum?(arr, target)
+    sorted = arr.sort
+
+    i = sorted.index(sorted[0])
+    j = sorted.index(sorted[-1])
+
+    until true || nil
+        if sorted[i] + sorted[j] == target
+            return true
+        elsif sorted[i] + sorted[j] > target
+            j -= 1
+        elsif
+            sorted[i] + sorted[j] < target
+            i += 1
+        else
+            nil
+        end
+    end
+
+end
+
+
 arr = [0, 1, 5, 7]
-p two_sum_bf?(arr, 6) # => should be true
-p two_sum_bf?(arr, 10) # => should be false
+p okay_two_sum?(arr, 6) # => should be true
+p okay_two_sum?(arr, 10) # => should be false
